990fc5febd8fd523093237d4a62335c4052c6ba9
DatAKAjezz <jezzthedead@gmail.com>
2024-12-27 15:30:19 +0700

    skeleton and packs

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mysql = require('mysql');
+const db = require('./src/config/db');
+
+const app = express();
+
+db.query('SHOW DATABASES;', (err, results) => {
+  if (err) throw err;
+  console.log('Danh sách databases:', results);
+  results.forEach((table)  => {console.log(table)})
+});
+
+app.get('/api/quizzes', (req, res) => {
+  db.query('SELECT * FROM quizzes', (err, result) => {
+    if (err) {
+      res.status(500).json({ error: 'Lỗi khi lấy dữ liệu' });
+      return;
+    }
+    res.json(result); 
+  });
+});
+
+app.listen(3001, () => {
+  console.log('Server đang chạy tại http://localhost:3001');
+})
+
---
+++ src/config/db.js
@@ -0,0 +1,18 @@
+const mysql = require('mysql');
+
+const connection = mysql.createConnection({
+  host: 'localhost',     
+  user: 'datdat',            
+  password: 'datdat2kk5',            
+  database: 'quiz_app'    
+});
+
+connection.connect((err) => {
+  if (err) {
+    console.error('Kết nối thất bại: ' + err.stack);
+    return;
+  }
+  console.log('Đã kết nối MySQL với ID: ' + connection.threadId);
+});
+
+module.exports = connection;
