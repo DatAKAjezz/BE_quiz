1e5cc8ca545f535f9778395ede6aa891c86dd646
Dat Hoang Tan <jezzthedead@gmail.com>
2025-01-11 16:43:54 +0700

    now can modify profile

--- app.js
+++ app.js
@@ -25,16 +25,16 @@ app.post('/login', (req, res) => {
   const { username, password } = req.body;
   const query = 'SELECT * FROM users WHERE username = ? AND password = ?'
   db.query(query, [username, password], (err, results) => {
-    if (err){
+    if (err) {
       console.log('Error: ', err);
-      return res.status(500).json({success: false, message: 'Internal server error login'})
+      return res.status(500).json({ success: false, message: 'Internal server error login' })
     }
-    if (results.length > 0){
-      const token = jwt.sign({id: results[0].id, username: results[0].username }, SECRET_KEY, {expiresIn: '1h'})
+    if (results.length > 0) {
+      const token = jwt.sign({ id: results[0].id, username: results[0].username }, SECRET_KEY, { expiresIn: '1h' })
       // console.log(token);
-      res.json({success: true, token, message: 'Login successfully.'})
-    }else{
-      res.status(401).json({success: false, message: 'Invalid credentials'})
+      res.json({ success: true, token, message: 'Login successfully.' })
+    } else {
+      res.status(401).json({ success: false, message: 'Invalid credentials' })
     }
   })
 })
@@ -56,37 +56,51 @@ app.post('/signup', (req, res) => {
 
 app.get('/dashboard', authenticationToken, (req, res) => {
 
-  const username = req.user.username;  
-  
-  const query = 'SELECT * FROM users WHERE username = ? LIMIT 1';
+  const username = req.user.username;
+
+  const query = `SELECT * FROM 
+                 users u JOIN user_introductions ui ON u.id = ui.user_id  
+                 WHERE username = ? LIMIT 1`;
   db.query(query, [username], (err, results) => {
     if (err) {
       console.log("Error: ", err);
       return res.status(500).json({
-        success: false, 
+        success: false,
         message: 'Internal server error when fetching user data'
       });
     }
-    
+
     if (results.length === 0) {
       return res.status(404).json({
         success: false,
         message: 'User not found'
-      }); 
+      });
     }
-
-    res.json({ 
-      success: true, 
+    res.json({
+      success: true,
       data: results
     });
   });
 });
 
-app.get('/library',authenticationToken, (req, res) => {
+app.get('/flashcardsets/all', (req, res) => {
+  const query = `select * from 
+                 flashcard_sets fs
+                 JOIN flashcards f ON f.set_id = fs.id`
+  db.query(query, [], (err, results) => {
+    if (err){
+      res.json({success: false, message: 'Fetching all flashcard sets failed'})
+      return;
+    }
+    res.json({success: true, data: results})
+  })
+})
+
+app.get('/library/history', authenticationToken, (req, res) => {
 
   const id = req.user.id;
   console.log("USER ID: ", id);
-  
+
   const query = `SELECT * from
                   users u 
                   JOIN study_history sh ON u.id = sh.user_id
@@ -94,23 +108,24 @@ app.get('/library',authenticationToken, (req, res) => {
                   WHERE u.id = ?`
 
   db.query(query, [id], (err, results) => {
-    if (err){
+    if (err) {
       console.log('Error: ', err);
-      return res.json({success: false, message: 'Internal server error at /library'})
+      return res.json({ success: false, message: 'Internal server error at /library' })
     }
     if (results.length === 0) {
       return res.status(404).json({
         success: false,
         message: 'History not found'
-      }); 
+      });
     }
     console.log('History: ', results);
-    res.json({ 
-      success: true, 
+    res.json({
+      success: true,
       data: results
     });
   })
 })
+
 
 app.get('/api/quizzes', (req, res) => {
   db.query('SELECT * FROM quizzes', (err, result) => {
@@ -118,14 +133,54 @@ app.get('/api/quizzes', (req, res) => {
       res.status(500).json({ error: 'Lỗi khi lấy dữ liệu' });
       return;
     }
-    res.json(result); 
+    res.json(result);
   });
 });
 
-  // app.get('/hello', (req, res) => {
-  //   console.log("hello")
-  //   res.status(500).json({message: 'hello'})
-  // })
+app.get('/api/contributions/:userId', (req, res) => {
+
+  const userId = req.params.userId;
+  // console.log('USER ID: ', userId);
+
+  const query = `select *  from
+                  users u JOIN contributions c ON u.id = c.user_id
+                  where u.id = ?`
+
+  db.query(query, [userId], (err, results) => {
+    if (err) {
+      console.log('Error: ', err);
+      res.json({ success: false, message: 'Error at contributions' })
+      return;
+    }
+
+    res.json({ success: true, data: results })
+
+  })
+
+})
+
+app.put('/modify/introduction', (req, res) => {
+
+  const { userId, message } = req.body;
+  if (!userId || !message) {
+    return res.status(400).json({ success: false, message: 'User ID and message are required' });
+  }
+
+  const query = `update user_introductions set readme = ? where user_id = ?`
+
+  db.query(query, [message, userId], (err, _) => {
+    if (err) {
+      res.json({ success: false, message: 'Error change user information.' })
+      return;
+    }
+    res.json({ success: true, message: 'User infomations updated successfully.' })
+  })
+})
+
+// app.get('/hello', (req, res) => {
+//   console.log("hello")
+//   res.status(500).json({message: 'hello'})
+// })
 
 app.listen(3001, () => {
   console.log('Server đang chạy tại http://localhost:3001');
